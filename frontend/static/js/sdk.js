e332f57d1e4366b8a835a187e14b4823e6155f18
adonelson <adonelson@affiniongroup.com>
2022-12-26 18:50:35 -0600

    added some checkers endpoints

--- frontend/static/js/sdk.js
+++ frontend/static/js/sdk.js
@@ -25,3 +25,43 @@ async function getLatestBlock() {
         console.error(error);
     }
 }
+
+// 
+// getCheckersSystemInfo returns the current state from the Cosmos Checkers blockchain
+// Usage:
+//      result = await getCheckersSystemInfo();
+async function getCheckersSystemInfo() {
+    try {
+        const response = await fetch(BlockchainHost+'/checkers/checkers/system_info');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// getCheckersStoredGames returns the stored games from the Cosmos Checkers blockchain
+// Usage:
+//      result = await getCheckersStoredGames();
+async function getCheckersStoredGames() {
+    try {
+        const response = await fetch(BlockchainHost+'/checkers/checkers/stored_game');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// getCheckersStoredGame returns the stored game of the provided index from the Cosmos Checkers blockchain
+// Usage:
+//      result = await getCheckersStoredGame(index);
+async function getCheckersStoredGame(index) {
+    try {
+        const response = await fetch(BlockchainHost+'/checkers/checkers/stored_game/'+index);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
